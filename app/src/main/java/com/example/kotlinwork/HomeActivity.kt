895b3abf13ed7f87761029ca2add9a3ec6c58443
SasoriWasHere <108181508+SasoriWasHere@users.noreply.github.com>
2023-10-22 00:18:59 +0800

    initial commit

--- app/src/main/java/com/example/kotlinwork/HomeActivity.kt
+++ app/src/main/java/com/example/kotlinwork/HomeActivity.kt
@@ -2,10 +2,25 @@ package com.example.kotlinwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btn_nav)
+        val navController = Navigation.findNavController(this,R.id.S_homeFragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
+
     }
 }
